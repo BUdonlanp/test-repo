1b2a2e574bffd87446be335006ca5966f3bc315f
donlanp <donlanp13@gmail.com>
2015-09-19 14:08:24 -0400

    Initial commit.

---
+++ test-project/src/main/java/edu/bu/donlanp/HelloWorld.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package edu.bu.donlanp;
+
+/**
+ * @author donlanp
+ *
+ */
+public class HelloWorld {
+
+	public static void main(String[] args){
+		System.out.println("Hello, world!");
+	}
+	
+}
